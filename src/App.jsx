3716a6feea30c4170a8bf64fef2b5c62fae3f517
gabicostagaber <gabicostagaber@gmail.com>
2025-05-02 19:26:19 +0000

    criação do arquivo cardfinal.jsx para receber as informaçoes do card final

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import illustrationThankYou from "./assets/illustration-thank-you.svg"
 import { useState } from "react";
 import { CardInicial } from "./components/CardInicial";
+import { CardFinal } from "./components/CardFinal";
 
 export function App() {
    const [nota, SetNota] = useState(0);
@@ -11,16 +11,7 @@ return (
     submited === false? (
     <CardInicial nota={nota} SetNota={SetNota} setSubmited={setSubmited}/>
 ) : (
-   <div  className="max-w-103 bg-gradient-dark p-6 rounded-2xl text-white font-overpass text-center">
-      <img className= "mx-auto mb-6" src={illustrationThankYou} alt="illustration Thank You" />
-
-      <p className="mb-6 text-orange bg-dark-blue w-fit mx-auto px-3 py-1.25 rounded-3xl text-sm">You selected {nota} out of 5</p>
-      
-      <h1 className="text-2xl font-bold mb-2.5">Thank you!</h1>
-
-     <p className="text-sm text-light-grey mb-6">Please let us know how we did with your support request. 
-    All feedback is appreciated to help us improve our offering!</p>
-   </div>
+  <CardFinal nota={nota}/>
 )
    )
 }
